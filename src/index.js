0ac60921571458ac52e3a3a2d2cd796e4f600df5
Sta11k <alexandiel1984@gmail.com>
2021-08-07 20:16:58 +0300

    templates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+import data from "./data/menu.json"
+import murkup from "./templates/markup.hbs"
+
+
+const menuList = document.querySelector(".js-menu")
+console.log(menuList);
+console.log(data);
+// console.log(murkup);
