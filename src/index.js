4fcaf4b55d9dcf0981a493a5072a426a9f24d201
Sta11k <alexandiel1984@gmail.com>
2021-08-08 19:58:01 +0300

    release-tets-1

--- src/index.js
+++ src/index.js
@@ -3,76 +3,56 @@ import dataMenu from "./data/menu.json"
 import menuMrpTemplate from "./templates/markup.hbs"
 const markup = menuMrpTemplate(dataMenu);
 
-const menuList = document.querySelector(".js-menu");
-const themeSwitch = document.querySelector("#theme-switch-toggle");
-const bodyBg = document.querySelector("body");
-
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
+
+const menuList = document.querySelector(".js-menu");
+const themeSwitch = document.querySelector("#theme-switch-toggle");
+const bodyBg = document.querySelector("body");
+
 menuList.insertAdjacentHTML('beforeend', markup);
 
-const theme = localStorage.getItem(`${Theme.DARK}`);
-const storageAudit = JSON.parse(localStorage.getItem(`${Theme.DARK}`));
 
-// function storageTheme() {
-//   if (theme) {
-    
-//   }
-//     bodyBg.classList.toggle(`${Theme.DARK}`);
-// };
+// const theme = localStorage.getItem(`${Theme.DARK}`);
+// const storageAudit = JSON.parse(localStorage.getItem(`${Theme.DARK}`));
 
-// storageTheme();
 themeSwitch.addEventListener("change", switcherBgColor);
-
-function switcherBgColor() {
-
- 
-    localStorage.setItem('theme', `${Theme.DARK}`)
-    bodyBg.classList.toggle(`${Theme.DARK}`);
-   
-
-};
- const check = themeSwitch.checked;
-console.log(check);
+themeSwitch.addEventListener("change", checkBgColorLocStor);
 
 
-//   const sliderSwitch = document.querySelector("#theme-switch-toggle");
-  
-//   const body = document.querySelector("body");
-  
 
+  function switcherBgColor() {
+      const check = themeSwitch.checked;
+      if (check) {
+    bodyBg.classList.add(Theme.DARK);
+    bodyBg.classList.remove(Theme.LIGHT);
+     } else {
+    bodyBg.classList.add(Theme.LIGHT);
+    bodyBg.classList.remove(Theme.DARK);
+     }
 
-//   sliderSwitch.addEventListener("change", checkColor);
-//   sliderSwitch.addEventListener("change", inLocaLeStorage);
+}
+
+   function checkBgColorLocStor() {
+    const check = themeSwitch.checked;
+    // console.log(check);
+    if (check) {
+      localStorage.setItem("theme", Theme.DARK);
+    } else {
+      localStorage.removeItem("theme");
+      localStorage.setItem("theme", Theme.LIGHT);
+    }
+}
+
   
-//   function checkColor() {
-//     const check = sliderSwitch.checked;
-  
-//     if (check) {
-//       body.classList.add(Theme.DARK);
-//       body.classList.remove(Theme.LIGHT);
-//     } else {
-//       body.classList.add(Theme.LIGHT);
-//       body.classList.remove(Theme.DARK);
-//     }
-//   }
-  
-//   function inLocaLeStorage() {
-//     const check = sliderSwitch.checked;
-  
-//     if (check) {
-//       localStorage.setItem("theme", Theme.DARK);
-//     } else {
-//       localStorage.removeItem("theme");
-//       localStorage.setItem("theme", Theme.LIGHT);
-//     }
-//   }
-  
-//   const changeLocalTheme = localStorage.getItem("theme");
-//   if (changeLocalTheme === Theme.DARK) {
-//     body.classList.add(Theme.DARK);
-//     sliderSwitch.checked = true;
-//   }
+const replaceLocalTheme = localStorage.getItem("theme");
+if (replaceLocalTheme === Theme.DARK) {
+    themeSwitch.checked = true;
+    bodyBg.classList.add(Theme.DARK);
+
+    
+}
+
