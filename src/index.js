e0687713f65350bde67d99d0ac4396a0a763408b
Sta11k <alexandiel1984@gmail.com>
2021-08-08 14:15:45 +0300

    shablon-V

--- src/index.js
+++ src/index.js
@@ -21,11 +21,77 @@ const Theme = {
  
 menuList.insertAdjacentHTML('beforeend', markup);
 
+
+
+
+
+const theme = localStorage.getItem(`${Theme.DARK}`);
+const storageAudit = JSON.parse(localStorage.getItem(`${Theme.DARK}`));
+
+// function storageTheme() {
+//   if (theme) {
+    
+//   }
+//     bodyBg.classList.toggle(`${Theme.DARK}`);
+// };
+
+// storageTheme();
 themeSwitch.addEventListener("change", switcherBgColor);
 
-function switcherBgColor(e) {
-    e.preventDefault();
+function switcherBgColor() {
+
+ 
+    localStorage.setItem('theme', `${Theme.DARK}`)
     bodyBg.classList.toggle(`${Theme.DARK}`);
+   
 
- };
+};
 
+
+
+
+
+console.log(theme);
+// const theme = localStorage.getItem(`${Theme.DARK}`);
+
+// function storageTheme() {
+//   if(storageAudit ++) 
+// // };
+
+
+
+//   const sliderSwitch = document.querySelector("#theme-switch-toggle");
+  
+//   const body = document.querySelector("body");
+  
+//   sliderSwitch.addEventListener("change", checkColor);
+//   sliderSwitch.addEventListener("change", inLocaLeStorage);
+  
+//   function checkColor() {
+//     const check = sliderSwitch.checked;
+  
+//     if (check) {
+//       body.classList.add(Theme.DARK);
+//       body.classList.remove(Theme.LIGHT);
+//     } else {
+//       body.classList.add(Theme.LIGHT);
+//       body.classList.remove(Theme.DARK);
+//     }
+//   }
+  
+//   function inLocaLeStorage() {
+//     const check = sliderSwitch.checked;
+  
+//     if (check) {
+//       localStorage.setItem("theme", Theme.DARK);
+//     } else {
+//       localStorage.removeItem("theme");
+//       localStorage.setItem("theme", Theme.LIGHT);
+//     }
+//   }
+  
+//   const changeLocalTheme = localStorage.getItem("theme");
+//   if (changeLocalTheme === Theme.DARK) {
+//     body.classList.add(Theme.DARK);
+//     sliderSwitch.checked = true;
+//   }
